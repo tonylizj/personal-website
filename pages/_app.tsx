0a4ef8d7b0888493231a75549583fb0e1d40a18d
Tony Li <tonyli8101@gmail.com>
2021-01-06 01:22:32 -0500

    remove outdated code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,49 +2,19 @@ import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { useEffect } from 'react';
 
-// interface requiredImage {
-//   format: string;
-//   height: number;
-//   width: number;
-//   src: string;
-//   toString: () => string;
-// }
-
-const App = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => { // TODO: make this work in firefox
-    // (window as any).preloadedImages = [];
-    // const preload = (...imageInputs: requiredImage[]) => {
-    //   imageInputs.forEach((image) => {
-    //     const newImage = new Image();
-    //     newImage.src = image.toString();
-    //     (window as any).preloadedImages.push(newImage);
-    //   });
-    // };
-
-    // preload(
-    //   require('../public/images/tower.jpeg?webp'), // eslint-disable-line
-    //   require('../public/images/night-sky.jpeg?webp'), // eslint-disable-line
-    //   require('../public/images/tree.jpeg?webp'), // eslint-disable-line
-    //   require('../public/images/flowerid-icon.png?webp'), // eslint-disable-line
-    //   require('../public/images/highlighter-icon.png?webp'), // eslint-disable-line
-    // );
-  }, []);
-
-  return (
-    <>
-      <Head>
-        {/* eslint-disable-next-line */}
-        <link rel="preload" href={require('../public/images/tower.jpeg?webp')} as="image" />
-        {/* eslint-disable-next-line */}
-        <link rel="preload" href={require('../public/images/night-sky.jpeg?webp')} as="image" />
-        {/* eslint-disable-next-line */}
-        <link rel="preload" href={require('../public/images/tree.jpeg?webp')} as="image" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-};
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      {/* eslint-disable-next-line */}
+      <link rel="preload" href={require('../public/images/tower.jpeg?webp')} as="image" />
+      {/* eslint-disable-next-line */}
+      <link rel="preload" href={require('../public/images/night-sky.jpeg?webp')} as="image" />
+      {/* eslint-disable-next-line */}
+      <link rel="preload" href={require('../public/images/tree.jpeg?webp')} as="image" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
