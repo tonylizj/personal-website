7ab86d900f7511b905e8bb64fb60f026e15d0fcf
Tony Li <tonyli8101@gmail.com>
2021-01-06 17:33:14 -0500

    testing no preload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => (
+const App = ({ Component, pageProps }: AppProps) => ( // TODO: fix flash of plain html before css loads
   <>
     <Head>
-      <link rel="preload" href="images/tower.webp" as="image" />
+      {/* <link rel="preload" href="images/tower.webp" as="image" />
       <link rel="preload" href="images/night-sky.webp" as="image" />
-      <link rel="preload" href="images/tree.webp" as="image" />
+      <link rel="preload" href="images/tree.webp" as="image" /> */}
     </Head>
     <Component {...pageProps} />
   </>
