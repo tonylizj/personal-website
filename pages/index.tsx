d7e9517e1ad1c5af0687c2f90864b423bc878a15
Tony Li <tonyli8101@gmail.com>
2021-05-15 19:02:55 -0400

    more mobile changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Container from 'react-bootstrap/Container';
-import { useMediaQuery } from 'react-responsive';
+
 import Jumbotron from '../components/Jumbotron';
 import Head from '../components/Head';
 import PageBase from '../components/PageBase';
@@ -12,27 +12,20 @@ const Home = () => {
   const subtitle = 'I\'m an undergraduate computer science student at the University of Waterloo.';
   const construction = '';
 
-  const isMobile = useMediaQuery({ maxWidth: 700 });
-  const isHuge = useMediaQuery({ minWidth: 2800 });
-
   return (
     <>
       <Head />
       <PageBase pageName="home">
         <Jumbotron className={styles.mainJumbo}>
-          {/* eslint-disable-next-line */}
-          <Container className={isMobile ? styles.insideMainJumbo : (isHuge ? styles.insideMainJumboHuge : styles.insideMainJumbo)}>
+          <Container className={styles.insideMainJumbo}>
             <Container className={styles.wrapper} fluid>
               <Container className={styles.titleWrapper} fluid>
-                {/* eslint-disable-next-line max-len */}
-                <Container className={isMobile ? styles.nextImageContainerMobile : styles.nextImageContainer}>
+                <Container className={styles.nextImageContainer}>
                   <Image className={styles.nextImage} src="/portrait.jpg" layout="fill" objectFit="cover" objectPosition="left" quality="60" loading="eager" priority />
                 </Container>
                 <Container className={styles.titleTextWrapper} fluid>
-                  {/* eslint-disable-next-line */}
-                  <h1 className={isMobile ? styles.titleMobile : (isHuge ? styles.titleHuge : styles.title)}>{title}</h1>
-                  {/* eslint-disable-next-line */}
-                  <h4 className={isMobile ? styles.subtitleMobile : (isHuge ? styles.subtitleHuge : styles.subtitle)}>{subtitle}</h4>
+                  <h1 className={styles.title}>{title}</h1>
+                  <h4 className={styles.subtitle}>{subtitle}</h4>
                 </Container>
               </Container>
             </Container>
